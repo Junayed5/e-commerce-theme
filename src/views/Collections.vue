5e1d9932aff5fa848e87c6b20f9da7a4c749f494
Junayed5 <mdjunayed352003@gmail.com>
2025-03-18 23:26:50 +0600

    collection product lounce

--- src/views/Collections.vue
+++ src/views/Collections.vue
@@ -21,8 +21,6 @@
             <label for="out-of-stock" class="pl-3 text-sm">Out of stock</label>
           </div>
         </div>
-
-        
       </div>
       <div>
         <div class="mb-5 mt-16">
@@ -31,21 +29,52 @@
         </div>
 
         <div>
-          <input type="range" name="price" id="price">
+          <input type="range" name="price" id="price" />
 
           <div class="flex items-center gap-3">
-            <input type="text" class="h-10 w-20 border bg-[url('https://cdn-icons-png.flaticon.com/128/9382/9382311.png')] bg-left bg-[auto_13px] bg-no-repeat pl-5">
+            <input
+              type="text"
+              class="h-10 w-20 border bg-[url('https://cdn-icons-png.flaticon.com/128/9382/9382311.png')] bg-left bg-[auto_13px] bg-no-repeat pl-5"
+            />
             <p>to</p>
-            <input type="text" class="h-10 w-20 border bg-[url('https://cdn-icons-png.flaticon.com/128/9382/9382311.png')] bg-left bg-[auto_13px] bg-no-repeat pl-5">
+            <input
+              type="text"
+              class="h-10 w-20 border bg-[url('https://cdn-icons-png.flaticon.com/128/9382/9382311.png')] bg-left bg-[auto_13px] bg-no-repeat pl-5"
+            />
           </div>
 
-          <button class="h-12 w-full bg-black hover:bg-white text-white hover:text-black border text-lg font-semibold my-5 uppercase">Apply</button>
+          <button
+            class="h-12 w-full bg-black hover:bg-white text-white hover:text-black border text-lg font-semibold my-5 uppercase"
+          >
+            Apply
+          </button>
         </div>
-
-
       </div>
     </div>
-    <div class="w-5/6">products</div>
+    <div class="px-8">
+      <div>
+        <h2 class="text-2xl font-bold mt-16 mb-5">EID COLLECTION 2025</h2>
+        <p class="border border-gray-200"></p>
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-4 my-10">
+        <div v-for="(product, index) in products" :key="index" class="">
+          <div>
+            <img :src="product.image" :alt="product.title" />
+            <p class="text-center text-sm py-1">{{ product.title }}</p>
+            <p class="text-center font-semibold py-2">
+              TK.{{ product.price }}.00
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-center my-5">
+        <button
+          class="h-10 w-40 border uppercase hover:bg-black hover:text-white font-semibold transition duration-300 delay-150"
+        >
+          Load More
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -53,6 +82,30 @@ export default {
   data() {
     return {
       products: [
+        {
+          image:
+            "https://dorjibari.com.bd/cdn/shop/files/1_4725c873-27d1-448a-bfe9-fe4e9fc00e14.jpg?v=1741153209",
+          hoverImage:
+            "https://dorjibari.com.bd/cdn/shop/files/2_74d40ec0-a31f-4995-887a-06b7b1efd8f5.jpg?v=1741153209",
+          title: "Panjabi:Semi Fit_Fil-a-Fil_329#4",
+          price: "2,490",
+        },
+        {
+          image:
+            "https://dorjibari.com.bd/cdn/shop/files/1_4725c873-27d1-448a-bfe9-fe4e9fc00e14.jpg?v=1741153209",
+          hoverImage:
+            "https://dorjibari.com.bd/cdn/shop/files/2_74d40ec0-a31f-4995-887a-06b7b1efd8f5.jpg?v=1741153209",
+          title: "Panjabi:Semi Fit_Fil-a-Fil_329#4",
+          price: "2,490",
+        },
+        {
+          image:
+            "https://dorjibari.com.bd/cdn/shop/files/1_4725c873-27d1-448a-bfe9-fe4e9fc00e14.jpg?v=1741153209",
+          hoverImage:
+            "https://dorjibari.com.bd/cdn/shop/files/2_74d40ec0-a31f-4995-887a-06b7b1efd8f5.jpg?v=1741153209",
+          title: "Panjabi:Semi Fit_Fil-a-Fil_329#4",
+          price: "2,490",
+        },
         {
           image:
             "https://dorjibari.com.bd/cdn/shop/files/1_4725c873-27d1-448a-bfe9-fe4e9fc00e14.jpg?v=1741153209",
