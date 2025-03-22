8e67a3ad6b9bf43c99ce5554b56979f24ff82129
Junayed5 <mdjunayed352003@gmail.com>
2025-03-22 23:12:17 +0600

    side product details done

--- src/views/product/ProductDetails.vue
+++ src/views/product/ProductDetails.vue
@@ -5,17 +5,17 @@
         <img class="size-[671px]" :src="product.image" alt="" />
       </div>
       <div class="mx-14">
-        <div>
-          <h3 class="text-xl font-semibold">{{ product.name }}</h3>
+        <div class="mt-20">
+          <h3 class="text-xl font-semibold mb-2">{{ product.name }}</h3>
           <p class="text-xl font-semibold">Tk {{ product.price }}.00</p>
 
-          <div>
+          <div class="my-10">
             <p class="text-xs" v-if="product.inStock">4 In Stock</p>
             <p class="text-xs text-red-600" v-else="!product.inStock">
               Out Of Stock
             </p>
           </div>
-          <div>
+          <div class="mt-2 mb-10">
             <p class="text-sm font-semibold">Size: M</p>
             <div class="flex gap-2">
               <div v-for="size in product.size">
@@ -24,7 +24,9 @@
             </div>
           </div>
 
-          <p class="text-sm">Subtotal: TK 2990.0</p>
+          <p class="text-sm mb-3">
+            Subtotal: TK {{ product.price * product.quantity }}.00
+          </p>
           <p class="text-sm font-semibold">Quantity:</p>
           <div class="flex items-center gap-5">
             <div class="flex gap-7 border items-center h-12 my-3 px-2">
@@ -42,9 +44,11 @@
               Add To Cart
             </button>
             <button class="">
-              <i class="pi pi-heart size-10 border rounded-full"></i>
+              <i
+                class="pi pi-heart border border-gray-300 rounded-full text-2xl p-3 hover:bg-black hover:text-white transition delay-100"
+              ></i>
             </button>
-            <button><i class="pi pi-share-alt"></i></button>
+            <button><i class="pi pi-share-alt text-2xl"></i></button>
           </div>
 
           <button
@@ -67,7 +71,7 @@ export default {
         image:
           "https://dorjibari.com.bd/cdn/shop/files/2_dcd02819-59aa-463d-af87-a98168261b03.jpg?v=1741688110",
         name: "Panjabi:Semi Fit_Jacquard_341#1",
-        price: "2,990",
+        price: 2990,
         size: ["M", "L", "XL", "XXL"],
         showImage: [],
         stock: 8,
